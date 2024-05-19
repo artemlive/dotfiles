0e9bfbbb79dddd7a0f98c994d209eff5f1afa92a
Artem Miroshnychenko <itsysadmin9@gmail.com>
2024-05-19 21:36:14 +0200

    fix(tmux): alacritty

--- nvim/lua/custom/plugins/bufferline.lua
+++ nvim/lua/custom/plugins/bufferline.lua
@@ -1,5 +1,7 @@
 return {
   'akinsho/bufferline.nvim',
+  --  'Theyashsawarkar/bufferline.nvim',
+
   event = 'VeryLazy',
   keys = {
     { '<leader>bp', '<Cmd>BufferLineTogglePin<CR>', desc = 'Toggle Pin' },
@@ -7,8 +9,6 @@ return {
     { '<leader>bo', '<Cmd>BufferLineCloseOthers<CR>', desc = 'Delete Other Buffers' },
     { '<leader>br', '<Cmd>BufferLineCloseRight<CR>', desc = 'Delete Buffers to the Right' },
     { '<leader>bl', '<Cmd>BufferLineCloseLeft<CR>', desc = 'Delete Buffers to the Left' },
-    --    { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
-    --    { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
     { '[b', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
     { ']b', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
   },
