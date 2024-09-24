2db94def9818d5eaa8041f396b8244670bd1934a
Artem Miroshnychenko <itsysadmin9@gmail.com>
2024-09-24 14:26:39 +0200

    feat(nvim): telescope file browser cur dir

--- nvim/lua/custom/plugins/telescope-file-browser.lua
+++ nvim/lua/custom/plugins/telescope-file-browser.lua
@@ -5,5 +5,11 @@ return {
     vim.keymap.set('n', '<space>fb', function()
       require('telescope').extensions.file_browser.file_browser()
     end)
+    vim.keymap.set('n', '<space>fc', function()
+      require('telescope').extensions.file_browser.file_browser {
+        path = vim.fn.expand '%:p:h', -- Set the path to the directory of the current file
+        select_buffer = true, -- Set select_buffer to true
+      }
+    end)
   end,
 }
